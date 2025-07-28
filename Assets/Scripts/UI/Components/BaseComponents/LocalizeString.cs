61955b156fdfaa43a230bf1a592804646325799c
Jisu_Na <>
2025-07-28 15:06:19 +0900

    [UI] 업그레이드 팝업 추가

--- Assets/Scripts/UI/Components/BaseComponents/LocalizeString.cs
+++ Assets/Scripts/UI/Components/BaseComponents/LocalizeString.cs
@@ -10,34 +10,15 @@ public class LocalizeString : MonoBehaviour
     [SerializeField]
     private string keyLocalize = "str_error";
 
-    [SerializeField]
-    private bool IsTextMeshPro = false;
 
     private void Start() {
         if(!Localizelist.Contains(this))
             Localizelist.Add(this);
-        var tmp = GetComponent<Text>();
         var tmppro = GetComponent<TextMeshProUGUI>();
         RefreshText();
     }
     public void RefreshText()
     {
-        if (!IsTextMeshPro)
-        {
-            var tmp = GetComponent<Text>();
-            if (tmp)
-            {
-                tmp.text = Tables.Instance.GetTable<Localize>().GetString(keyLocalize);
-            }
-            else
-            {
-                var label = GetComponent<Text>();
-                if (label)
-                    label.text = Tables.Instance.GetTable<Localize>().GetString(keyLocalize);
-            }
-        }
-        else
-        {
             var tmppro = GetComponent<TextMeshProUGUI>();
             if (tmppro)
             {
@@ -49,38 +30,18 @@ public class LocalizeString : MonoBehaviour
                 if (label)
                     label.text = Tables.Instance.GetTable<Localize>().GetString(keyLocalize);
             }
-        }
+        
     }
   
     public void SetText(string txt)
     {
-        if (!IsTextMeshPro)
-        {
-            var tmp = GetComponent<Text>();
-            if (tmp)
-            {
-                tmp.text = txt;
-            }
-            else
-            {
-                var label = GetComponent<Text>();
-                if (label)
-                    label.text = txt;
-            }
-        }
-        else
-        {
+       
+         
             var tmppro = GetComponent<TextMeshProUGUI>();
             if (tmppro)
             {
                 tmppro.text = txt;
             }
-            else
-            {
-                var label = GetComponent<TextMeshProUGUI>();
-                if (label)
-                    label.text = txt;
-            }
-        }
+        
     }
 }
